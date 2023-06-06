0f531e6272b0b5df21906df622a38ed2db63b995
CorryJ <47356447+CorryJ@users.noreply.github.com>
2023-06-06 14:49:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="The SEO Works chat robot", page_icon="https://www
         'About': "Let us know what you think of the app?"
     })
 
-st.markdown("<h2 style='text-align: left;'>The SEO Works chat robot</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: left;'>The SEO Works OptiBot</h2>", unsafe_allow_html=True)
 
 tab1, tab2, tab3 = st.tabs(["Response builder", "Output", "Guide"])
 
